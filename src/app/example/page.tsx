9fad100a097eee8db2c97cc4b85d9d9e23ed7610
Fernando Michea <fmicheam@gmail.com>
2024-01-23 02:46:38 -0300

    cambios

--- src/app/example/page.tsx
+++ src/app/example/page.tsx
@@ -1,19 +1,10 @@
+import Intro from "@/section/Home/Intro";
 import Image from "next/image";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        
-        <h1 className="text-4xl font-bold">Cortito</h1>
-
-        <Image
-        src="/cortito.png"
-        alt="Vercel Logo"
-        className="dark:invert"
-        width={100}
-        height={24}
-        priority
-      />
+        <main className="flex min-h-screen flex-col items-center justify-between ">
+          <Intro />
       </main>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,14 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/section/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
+      colors: {
+        blanco: 'var(--foreground-rgb)',
+        azul: 'var(--background-start-rgb)',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
