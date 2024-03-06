c2fee82daf3ca148699de722a99fbc7ab7fe1764
Fernando Michea <fmicheam@gmail.com>
2024-03-05 22:39:59 -0300

    cambios

--- src/utils/axios.tsx
+++ src/utils/axios.tsx
@@ -1,10 +1,24 @@
 import axios from "axios";
-import { HOST_API } from "@/config_global";
+import { HOST_API,USER_API,PASSWORD_API } from "@/config_global";
+
+
+
+
+let user = USER_API;
+let password = PASSWORD_API;
+
 
 
 export const api = axios.create({
   baseURL: HOST_API,
   headers: {
     "Content-Type": "application/json",
+    "Authorization": "Basic " + btoa(`${user}:${password}`),
   },
 });
+
+
+export const endpoints = {
+//crear shroturl
+ crearShortUrl: "/shorturl",
+};
