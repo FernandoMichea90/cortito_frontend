d1384f35e534f43505bc4875993ed2e453f7e0a1
Fernando Michea <fmicheam@gmail.com>
2024-03-08 23:49:45 -0300

    login alert terminado

--- src/section/Header.tsx
+++ src/section/Header.tsx
@@ -2,8 +2,12 @@
 'use client'
 
 import React from 'react';
+import LoginModal from '@/components/LoginModall'
 
 const Header: React.FC = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return (
         <header className="bg-azul text-white p-4 fixed top-0 left-0 right-0 z-50">
             <div className="container mx-auto flex justify-between items-center">
@@ -14,11 +18,12 @@ const Header: React.FC = () => {
                         className="h-12" />
                 </div>
                 <div>
-                    <button className="bg-white text-azul px-4 py-2 rounded-md">
+                    <button onClick={handleOpen} className="bg-white text-azul px-4 py-2 rounded-md">
                         Iniciar Sesión
                     </button>
                 </div>
             </div>
+            <LoginModal open={open}  handleClose={handleClose} />
         </header>
     );
 };
